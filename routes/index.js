7d67e42ce0c1fda8cca5d509cab54f6277c6e8b1
Noroff BED <124564549+noroff-bed1@users.noreply.github.com>
2023-03-29 13:26:27 +0200

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = new express.Router();
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  let my_file = await s3.getObject({
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json",
+  }).promise()
+  const result = JSON.parse(my_file.Body)?.content
+  if(result == null) {
+    res.json({
+      status: "fail"
+    });  
+  }
+  else {
+    res.json({
+      status: "success",
+      content: result,
+    });  
+  }
+});
+
+router.post("/", async (req, res) => {
+  const {content} = req.body;
+  const contentObj = {
+    content: content
+  }
+  await s3.putObject({
+    Body: JSON.stringify(contentObj, null, 2),
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "content.json",
+  }).promise()
+  res.json({
+    status: "success",
+    content: content,
+  });
+});
+
+module.exports = router;
